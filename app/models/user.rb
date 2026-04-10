55ce4adbd59bf92ff5e037a23112fcf3db1ea93f
aqsazulfiqar538 <aqsa.zulfiqar@devsinc.com>
2026-04-11 01:54:57 +0500

    gemini fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, presence: true, length: { maximum: 20 }
-  validates :last_name, presence: true, length: { maximum: 20 }
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
   validates :phone_number, presence: true, format: { with: /\A[+]?[\d\s\-().]{7,15}\z/, message: "is not valid" }
   validates :date_of_birth, presence: true
 
