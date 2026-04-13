08332f7571c8f8e1763f33d350bd4b02bdbbefe9
aqsazulfiqar538 <aqsa.zulfiqar@devsinc.com>
2026-04-13 19:45:01 +0500

    add concern and detail to recommendation model

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,3 +1,26 @@
 class Recommendation < ApplicationRecord
+  CATEGORIES = %w[food travel medication].freeze
+
+  include Categorizable
+
   belongs_to :user
+
+  enum :action, {
+    none:                 0,
+    review_spending:      1,
+    set_budget_limit:     2,
+    seek_alternatives:    3,
+    urgent_intervention:  4
+  }
+
+  ACTION_LABELS = {
+    "none"                => "Looks good",
+    "review_spending"     => "Review your spending",
+    "set_budget_limit"    => "Set a budget limit",
+    "seek_alternatives"   => "Find cheaper alternatives"
+  }.freeze
+
+  def action_label
+    ACTION_LABELS[action] || "No action"
+  end
 end
