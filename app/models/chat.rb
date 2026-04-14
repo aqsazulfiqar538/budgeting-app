04ca4afd6d0cf0242a8e6400c663ca6cc79ac96f
aqsazulfiqar538 <aqsa.zulfiqar@devsinc.com>
2026-04-14 20:40:30 +0500

    migrate ruby_llm migrations

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: chats
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  model_id   :integer
+#
+# Indexes
+#
+#  index_chats_on_model_id  (model_id)
+#
+
 class Chat < ApplicationRecord
   acts_as_chat
 end
